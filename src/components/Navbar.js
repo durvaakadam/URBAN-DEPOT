43bbbd8c6ed911ff9f33a48e051168ad1119ad18
durvaakadam <durvakadam204@gmail.com>
2024-11-12 20:07:42 +0530

    NAVBAR HEIGHT

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,7 +62,7 @@ const Navbar = ({ userEmail }) => {
       <i className="fas fa-question-circle" style={{ color: 'white', marginRight: '12px' }}></i> FAQ
     </Link>
     <button className="urban-navbar-logout-btn" onClick={handleLogout}>
-      <i className="fas fa-sign-out-alt" style={{ color: 'white', marginRight: '12px' }}></i> Logout
+      <i className="fas fa-sign-out-alt" style={{ color: 'white', marginRight: '13px' }}></i> Logout
     </button>
   </div>
 )}
